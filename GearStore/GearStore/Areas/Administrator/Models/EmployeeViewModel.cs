173e9c57705c4b9064748ebe554aa59da855e1ab
akiratatsuhisa <akiratatsuhisa@gmail.com>
2019-04-11 00:20:18 +0900

    DisplayNameFor Prop Model

--- GearStore/GearStore/Areas/Administrator/Models/EmployeeViewModel.cs
+++ GearStore/GearStore/Areas/Administrator/Models/EmployeeViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
@@ -8,9 +9,11 @@ namespace GearStore.Areas.Administrator.Models
 {
     public class EmployeeSignInViewModel
     {
+        [DisplayName("Tài khoản")]
         [Required]
         [DataType(DataType.Text)]
         public string Username { get; set; }
+        [DisplayName("Mật khẩu")]
         [Required]
         [DataType(DataType.Password)]
         public string Password { get; set; }
--- GearStore/GearStore/Models/AccountViewModel.cs
+++ GearStore/GearStore/Models/AccountViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
@@ -8,9 +9,11 @@ namespace GearStore.Models
 {
     public class SignInViewModel
     {
+        [DisplayName("Tài khoản")]
         [Required]
         [DataType(DataType.Text)]
         public string Username { get; set; }
+        [DisplayName("Mật khẩu")]
         [Required]
         [DataType(DataType.Password)]
         public string Password { get; set; }
@@ -18,13 +21,17 @@ namespace GearStore.Models
 
     public class SignUpViewModel
     {
+        [DisplayName("Mã khách hàng")]
         public int CustomerID { get; set; }
         [Required]
         [DataType(DataType.Text)]
+        [DisplayName("Tài khoản")]
         public string Username { get; set; }
+        [DisplayName("Mật khẩu")]
         [Required]
         [DataType(DataType.Password)]
         public string Password { get; set; }
+        [DisplayName("Xác nhận mật khẩu")]
         [DataType(DataType.Password)]
         [Compare("Password")]
         public string ConfirmPassword { get; set; }
@@ -33,37 +40,51 @@ namespace GearStore.Models
         public string Email { get; set; }
         [Required]
         [DataType(DataType.Text)]
+        [DisplayName("Họ và tên")]
         public string FullName { get; set; }
+        [DisplayName("Ngày sinh")]
         [DataType(DataType.Date)]
         public DateTime? BirthDate { get; set; }
+        [DisplayName("Giới tính")]
         public bool Gender { get; set; }
+        [DisplayName("Địa chỉ")]
         [DataType(DataType.MultilineText)]
         public string Address { get; set; }
+        [DisplayName("Số điện thoại")]
         [Phone]
         public string PhoneNumber { get; set; }
     }
     public class AcccountDetailsViewModel
     {
+        [DisplayName("Mã khách hàng")]
         public int CustomerID { get; set; }
+        [DisplayName("Tài khoản")]
         public string Username { get; set; }
+        [DisplayName("Mật khẩu")]
         [Required]
         [DataType(DataType.Password)]
         public string Password { get; set; }
+        [DisplayName("Xác nhận mật khẩu")]
         [DataType(DataType.Password)]
         [Compare("Password")]
         public string ConfirmPassword { get; set; }
         [Required]
         [EmailAddress]
         public string Email { get; set; }
+        [DisplayName("Họ và tên")]
         [Required]
         [DataType(DataType.Text)]
         public string FullName { get; set; }
+        [DisplayName("Ngày sinh")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime? BirthDate { get; set; }
+        [DisplayName("Giới tính")]
         public bool Gender { get; set; }
+        [DisplayName("Địa chỉ")]
         [DataType(DataType.MultilineText)]
         public string Address { get; set; }
+        [DisplayName("Số điện thoại")]
         [Phone]
         public string PhoneNumber { get; set; }
     }
